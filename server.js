b7870245107bfbc7fb2594d738b55cfd831fd3d1
HwaJeong Kang <99319638+7rohj@users.noreply.github.com>
2023-03-06 14:35:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => console.log(`${port}포트입니다.`));
 
-app.get("/", (req,res) => {  
-	res.sendFile(__dirname + '/index.html'); 
+app.get("/add", (req,res) => {  
+	res.sendFile(__dirname + '/add.html'); 
 	});
 
 app.post("/add", (req,res) => {  
